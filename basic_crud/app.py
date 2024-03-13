8681c457523b2b6b0c7cf57883156b0cc09fa87f
Josh Wenner <68701564+Joshwen7947@users.noreply.github.com>
2024-03-13 21:30:50 +0700

    Update app.py
    No need for url_for anymore

--- basic_crud/app.py
+++ basic_crud/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request,redirect
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
